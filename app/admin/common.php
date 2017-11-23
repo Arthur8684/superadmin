649f73dd9533017d721fa535f9c2dc2f9acf2c31
Arthur8684 <15834068684@163.com>
2017-11-23 14:13:16 +0800

    modify admin public header

--- app/admin/common.php
+++ app/admin/common.php
@@ -1,5 +1,10 @@
 <?php
 use think\Response;
+use think\Db;
+/**
+ * @param $int
+ * @return bool|string
+ */
 function get_sex($int)
 {
     switch($int)
@@ -14,46 +19,11 @@ function get_sex($int)
             return false;
     }
 }
-/**
- * 框架内部默认ajax返回
- * @param string $msg      提示信息
- * @param string $redirect 重定向类型 current|parent|''
- * @param string $alert    父层弹框信息
- * @param bool $close      是否关闭当前层
- * @param string $url      重定向地址
- * @param string $data     附加数据
- * @param int $code        错误码
- * @param array $extend    扩展数据
- */
-function ajax_return_adv($msg = '操作成功', $redirect = 'parent', $alert = '', $close = false, $url = '', $data = '', $code = 0, $extend = [])
-{
-    $extend['opt'] = [
-        'alert'    => $alert,
-        'close'    => $close,
-        'redirect' => $redirect,
-        'url'      => $url,
-    ];
 
-    return ajax_return($data, $msg, $code, $extend);
-}
-/**
- * 返回错误json信息
- */
-function ajax_return_adv_error($msg = '', $code = 1, $redirect = '', $alert = '', $close = false, $url = '', $data = '', $extend = [])
-{
-    return ajax_return_adv($msg, $alert, $close, $redirect, $url, $data, $code, $extend);
-}
-/**
- * ajax数据返回，规范格式
- * @param array $data   返回的数据，默认空数组
- * @param string $msg   信息
- * @param int $code     错误码，0-未出现错误|其他出现错误
- * @param array $extend 扩展数据
- */
-function ajax_return($data = [], $msg = "", $code = 0, $extend = [])
-{
-    $ret = ["code" => $code, "msg" => $msg, "data" => $data];
-    $ret = array_merge($ret, $extend);
 
-    return Response::create($ret, 'json');
+function admin_info()
+{
+    $id = session('admin.id');
+    $info = Db::name('admin')->where('id',$id)->find();
+    return $info;
 }
