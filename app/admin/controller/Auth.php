ba91f14bb551ee0c1c28422c614f18437128e799
Arthur8684 <15834068684@163.com>
2017-11-29 14:41:24 +0800

    modify auth controller and edit_auth_group template

--- app/admin/controller/Auth.php
+++ app/admin/controller/Auth.php
@@ -13,6 +13,18 @@ use think\Loader;
 use think\util\AdminParent;
 
 class Auth extends AdminParent{
+    //菜单列表
+    private function getMenu()
+    {
+        $menu = Db::name('menu')->where('status',1)->where('parent_id',0)->select();
+        foreach($menu as &$v)
+        {
+            $v['child_menu'] = Db::name('menu')->where('status',1)->where('parent_id',$v['menu_id'])->select();
+        }
+        unset($v);
+        return $menu;
+    }
+    //权限组列表
     public function index(){
         $auth_list = Db::name('auth')->where('status',1)->select();
         $this->assign('auth_list',$auth_list);
@@ -50,13 +62,7 @@ class Auth extends AdminParent{
                 $this->error('权限组添加失败');
             }
         }
-        $menu = Db::name('menu')->where('status',1)->where('parent_id',0)->select();
-        foreach($menu as &$v)
-        {
-            $v['child_menu'] = Db::name('menu')->where('status',1)->where('parent_id',$v['menu_id'])->select();
-        }
-        unset($v);
-        $this->assign('menu',$menu);
+        $this->assign('menu',$this->getMenu());
         return $this->fetch('add_auth_group');
     }
     //删除权限组
@@ -87,15 +93,46 @@ class Auth extends AdminParent{
                 $this->error('非法操作');
             }
             $auth_info = Db::name('auth')->where('status',1)->where('auth_id',$auth_id)->find();
+            if(stripos($auth_info['menu_auth_id'],',') !== false)
+            {
+                $auth_info['menu_auth_id'] = explode(',',$auth_info['menu_auth_id']);
+            }
+            else
+            {
+                $auth_info['menu_auth_id'] = [$auth_info['menu_auth_id']];
+            }
+            $this->assign('auth_id',$auth_id);
             $this->assign('auth_info',$auth_info);
         }
-        $menu = Db::name('menu')->where('status',1)->where('parent_id',0)->select();
-        foreach($menu as &$v)
+        if($this->request->isPost())
         {
-            $v['child_menu'] = Db::name('menu')->where('status',1)->where('parent_id',$v['menu_id'])->select();
+            $data = $this->request->param();
+            $validate = Loader::validate('Auth');
+            if(!$validate->check($data))
+            {
+                $this->error($validate->getError());
+            }
+            $edit_data['auth_title'] = $data['auth_title'];
+            $edit_data['auth_id'] = $data['auth_id'];
+            unset($data['auth_title']);
+            unset($data['auth_id']);
+            $menu_auth_id = implode(',',$data);
+            if(empty($menu_auth_id))
+            {
+                $this->error('请选择该权限组菜单');
+            }
+            $edit_data['menu_auth_id'] = $menu_auth_id;
+            $edit = Db::name('auth')->update($edit_data);
+            if(!empty($edit))
+            {
+                $this->success('修改成功');
+            }
+            else
+            {
+                $this->error('修改失败');
+            }
         }
-        unset($v);
-        $this->assign('menu',$menu);
+        $this->assign('menu',$this->getMenu());
         return $this->fetch('edit_auth_group');
     }
 }
